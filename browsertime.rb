f63380230d453479b3d1184618c679760dd1aa9f
Tobias Lidskog <tobias@lidskog.se>
2013-11-09 21:38:13 +0100

    Add dependency on chromedriver.

--- browsertime.rb
+++ browsertime.rb
@@ -5,6 +5,8 @@ class Browsertime < Formula
   url 'https://github.com/tobli/browsertime/releases/download/browsertime-0.3/browsertime-0.3.zip'
   sha1 '82ad5cad81eacf9641227c94ac51a717d841f644'
 
+  depends_on 'chromedriver'
+
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
